28669e2e2d4a68e6790a9c97b06bc99dd23365d1
Kyubin Lee <binkalos@naver.com>
2018-11-06 20:04:12 +0900

    #7 add activity of result, splash

---
+++ app/src/main/java/com/example/leekyubin/myapplication/ResultActivity.java
@@ -0,0 +1,13 @@
+package com.example.leekyubin.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {//onCreate=resultActivity가 처음 만들어질때 얘를 시작하세요
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
